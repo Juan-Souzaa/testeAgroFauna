94630f66bf52bfcad63db390e41fda9bf23c6aff
Juan-Souzaa <souzajuan430@gmail.com>
2026-03-22 12:33:09 -0300

    feat: adiciona funcionalidades de edição, atualização e exclusão de livros

--- app/Http/Controllers/LivroController.php
+++ app/Http/Controllers/LivroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ListarLivrosRequest;
 use App\Http\Requests\StoreLivroRequest;
+use App\Http\Requests\UpdateLivroRequest;
 use App\Models\Categoria;
 use App\Models\Livro;
 use App\Services\LivroService;
@@ -36,5 +37,35 @@ class LivroController extends Controller
         return redirect()
             ->route('livros.index')
             ->with('success', 'Livro cadastrado com sucesso.');
+    }
+
+    public function edit(Livro $livro): Response
+    {
+        $this->authorize('update', $livro);
+
+        return Inertia::render('Livros/Edit', [
+            'livro' => $livro,
+            'categorias' => Categoria::query()->orderBy('nome')->get(['id', 'nome']),
+        ]);
+    }
+
+    public function update(UpdateLivroRequest $request, Livro $livro, LivroService $livros)
+    {
+        $livros->atualizar($livro, $request->validated());
+
+        return redirect()
+            ->route('livros.index')
+            ->with('success', 'Livro atualizado com sucesso.');
+    }
+
+    public function destroy(Livro $livro, LivroService $livros)
+    {
+        $this->authorize('delete', $livro);
+
+        $livros->apagar($livro);
+
+        return redirect()
+            ->route('livros.index')
+            ->with('success', 'Livro excluído com sucesso.');
     }
 }
