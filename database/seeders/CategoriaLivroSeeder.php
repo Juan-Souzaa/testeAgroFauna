84ef3ae5aa713bed1761548b184948281c077d59
Juan-Souzaa <souzajuan430@gmail.com>
2026-03-21 23:44:45 -0300

    feat: seeders de categorias, livros e utilizadores

---
+++ database/seeders/CategoriaLivroSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categoria;
+use App\Models\Livro;
+use Illuminate\Database\Seeder;
+
+class CategoriaLivroSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $ficcao = Categoria::query()->firstOrCreate(['nome' => 'Ficção']);
+        $tecnico = Categoria::query()->firstOrCreate(['nome' => 'Técnico']);
+        $biografia = Categoria::query()->firstOrCreate(['nome' => 'Biografia']);
+
+        Livro::factory()->count(2)->create(['categoria_id' => $ficcao->id]);
+        Livro::factory()->count(2)->create(['categoria_id' => $tecnico->id]);
+        Livro::factory()->count(1)->create(['categoria_id' => $biografia->id]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,16 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call(RolePermissionSeeder::class);
-
-        User::factory()->admin()->create()->assignRole('admin');
-        User::factory()->editor()->create()->assignRole('editor');
+        $this->call([
+            RolePermissionSeeder::class,
+            CategoriaLivroSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
